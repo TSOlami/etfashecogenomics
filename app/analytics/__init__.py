c3e6928c140b97a490b14cde8592c3b336be6188
Tijani Saheed Olalekan <113607787+TSOlami@users.noreply.github.com>
2025-06-18 11:26:14 +0100

    Deleted __init__.py

--- app/analytics/__init__.py
+++
@@ -1,1 +0,0 @@
-# Analytics App - Data analysis functionality
--- app/analytics/views.py
+++
@@ -1,6 +0,0 @@
-"""
-Views for the analytics app.
-"""
-from django.shortcuts import render
-
-# Analytics views will be implemented as we add analysis features
